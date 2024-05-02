c6b7bfeb51d364c304a14f2d204a44efeb901c75
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-05-01 18:56:13 -0700

    Initial Commit

---
+++ Othello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Othello
+//
+//  Created by DJ Cruz on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Othello/OthelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthelloApp.swift
+//  Othello
+//
+//  Created by DJ Cruz on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct OthelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
