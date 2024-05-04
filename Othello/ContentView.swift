470c9fc5c00e8b498ccc42b4a6a8c923de155b70
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-05-04 02:12:53 -0700

    Styled the main menu and started the HowToPlayView

--- Othello/ContentView.swift
+++ Othello/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+struct TealButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 33.0/255.0, green: 77.0/255.0, blue: 114.0/255.0))
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "face.smiling")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("DJ can you see this?")
+        NavigationStack {
+            ZStack {
+                Color(red: 0.172, green: 0.463, blue: 0.584)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("OthelloLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                        .foregroundStyle(.tint)
+                    
+                    NavigationLink {
+                        GameView() // but AI
+                    } label: {
+                        Text("Player Vs. AI")
+                    }
+                    .buttonStyle(TealButton())
+                    
+                    NavigationLink {
+                        //GameView() but two player
+                    } label: {
+                        Text("Player Vs. Player")
+                    }
+                    .buttonStyle(TealButton())
+                    
+                    NavigationLink {
+                        HowToPlayView()
+                    } label: {
+                        Text("How To Play")
+                    }
+                    .buttonStyle(TealButton())
+                    
+                    NavigationLink {
+                        //RecordsView
+                    } label: {
+                        Text("Records")
+                    }
+                    .buttonStyle(TealButton())
+                }
+            }
         }
-        .padding()
     }
 }
 
--- Othello/HowToPlayView.swift
+++ Othello/HowToPlayView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct HowToPlayView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.172, green: 0.463, blue: 0.584)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Under Construction!")
+            }
+        }
     }
 }
 
