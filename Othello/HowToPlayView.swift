dada39e69274755fc9cc87c842dd66ca16ebdfe8
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-05-04 02:11:52 -0700

    Styled the Main Menu and started HowToPlayView

---
+++ Othello/HowToPlayView.swift
@@ -0,0 +1,18 @@
+//
+//  HowToPlayView.swift
+//  Othello
+//
+//  Created by DJ Cruz on 5/4/24.
+//
+
+import SwiftUI
+
+struct HowToPlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HowToPlayView()
+}
